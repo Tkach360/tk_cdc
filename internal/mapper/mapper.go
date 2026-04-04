585115a95be01646f13a5234807b68cfe3816a9d
Tkach360 <tkacenk002@mail.ru>
2026-04-04 15:02:52 +0700

    сделал replicator.New

--- internal/mapper/mapper.go
+++ internal/mapper/mapper.go
@@ -2,3 +2,13 @@
 
 // логика маппинга полей postgres и ключей redis
 package mapper
+
+import "github.com/Tkach360/tk_cdc/internal/config"
+
+type Mapper struct {
+}
+
+func New(mrules []config.MappingRule) *Mapper {
+	// TODO: сделать mapper.New
+	return nil
+}
--- internal/replicator/replicator.go
+++ internal/replicator/replicator.go
@@ -5,16 +5,50 @@ package replicator
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Tkach360/tk_cdc/internal/config"
+	"github.com/Tkach360/tk_cdc/internal/mapper"
+	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
 )
 
 type Replicator struct {
+	cfg      *config.Config
+	pgConfig *pgx.ConnConfig
+	redis    *redis.Client
+	mapper   *mapper.Mapper
+	slotName string
+	plugin   string
 }
 
 func New(cfg *config.Config) (*Replicator, error) {
-	// TODO: сделать replicator.New
-	return &Replicator{}, nil
+
+	pgConfig, err := pgx.ParseConfig(cfg.Postgres.DSN)
+	if err != nil {
+		return nil, fmt.Errorf("parse postgres DSN: %w", err)
+	}
+
+	pgConfig.RuntimeParams["replication"] = "database"
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     cfg.Redis.Addr,
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.DB,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("redis ping failed: %w", err)
+	}
+
+	return &Replicator{
+		cfg:      cfg,
+		pgConfig: pgConfig,
+		redis:    rdb,
+		mapper:   mapper.New(cfg.Mapping),
+		slotName: cfg.Postgres.ReplicationSlot,
+		plugin:   cfg.Postgres.Plugin,
+	}, nil
 }
 
 // основной цикл работы replicator

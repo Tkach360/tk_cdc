d2b8e8207a3c17912c13115ab3eb2f9395dbf1af
Tkach360 <tkacenk002@mail.ru>
2026-04-04 14:45:30 +0700

    минимальная реализация replicator.New

--- internal/replicator/replicator.go
+++ internal/replicator/replicator.go
@@ -2,3 +2,25 @@
 
 // логика чтения WAL
 package replicator
+
+import (
+	"context"
+
+	"github.com/Tkach360/tk_cdc/internal/config"
+)
+
+type Replicator struct {
+}
+
+func New(cfg *config.Config) (*Replicator, error) {
+	// TODO: сделать replicator.New
+	return &Replicator{}, nil
+}
+
+// основной цикл работы replicator
+// - читает WAL
+// - обновляет кеш
+func (r *Replicator) Run(ctx context.Context) error {
+	// TODO: сделать метод Replicator.Run
+	return nil
+}

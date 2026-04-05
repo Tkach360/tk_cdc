74878d8577f751c4fdc101cbfca178a132d010c9
Tkach360 <tkacenk002@mail.ru>
2026-04-05 19:36:59 +0700

    доделал структуру Mapper

--- internal/mapper/mapper.go
+++ internal/mapper/mapper.go
@@ -4,16 +4,42 @@
 package mapper
 
 import (
+	"strings"
+
 	"github.com/Tkach360/tk_cdc/internal/config"
 	"github.com/jackc/pglogrepl"
 )
 
 type Mapper struct {
+	// правила маппинга
+	// - key - имя отображения (таблицы) в виде схема.имя
+	// - value - собственно правило
+	rules map[string]config.MappingRule
+
+	// хеш-таблица данных об отношениях
+	// - key - RelationID
+	// - value - *pglogrepl.RelationMessage
+	relData map[uint32]*pglogrepl.RelationMessage
 }
 
-func New(mrules []config.MappingRule) *Mapper {
-	// TODO: сделать mapper.New
-	return nil
+func New(rules []config.MappingRule) *Mapper {
+
+	mrules := make(map[string]config.MappingRule)
+	for _, rule := range rules {
+		key := rule.Table
+
+		// нормализуем, чтобы обязательно была схема, если схема не указана, то схема public
+		// TODO: не забыть добавить в документацию, что нужно указывать схему иначе схема public
+		if !strings.Contains(rule.Table, ".") {
+			key = "public." + rule.Table
+		}
+		mrules[key] = rule
+	}
+
+	return &Mapper{
+		mrules,
+		make(map[uint32]*pglogrepl.RelationMessage),
+	}
 }
 
 // закешировать данные об отображении (таблице)
